64af02cf82728ef4847022de7f9ef1c28a52b4c4
lillyphan <lphan24@kentdenver.org>
2023-02-23 11:55:24 -0700

    Add constructor and hashCode(). Test values for hashCode().

--- src/testDriver.java
+++ src/testDriver.java
@@ -1,5 +1,15 @@
 public class testDriver {
     public static void main(String[] args) {
+        HashTable<String> ht = new HashTable<>(10);
+        System.out.println(ht.hashCode("aaaa"));
+        System.out.println(ht.hashCode("aaab"));
+        System.out.println(ht.hashCode("bbbb"));
+        System.out.println(ht.hashCode("cccc"));
+        System.out.println(ht.hashCode("dddd"));
+        System.out.println(ht.hashCode("House"));
+        System.out.println(ht.hashCode("housE"));
 
+        //where it maxes out
+        System.out.println(ht.hashCode("superduperduperduperdddd"));
     }
 }
