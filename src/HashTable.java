72069d6dad73010dfdc4bf783529e2b39b253ffe
lillyphan <lphan24@kentdenver.org>
2023-02-26 09:47:08 -0700

    Adjust constructor to fill HashTable. Write put, get, and toString (for testing purposes) functions. Test put and toString.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,22 +1,33 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
-public class HashTable<E>{
-    private ArrayList<E> table;
+public class HashTable{
+    private ArrayList<String> table;
+    private int size;
 
     //initializes an array of size capacity
     public HashTable(int capacity){
-        table = new ArrayList<>(capacity);
+        table = new ArrayList<>();
+        size = capacity;
+        for (int i = 0; i < capacity; i++) {
+            table.add(null);
+        }
     }
 
     //put hashes the key to an index in your array, and places the value there. Fails if there are collisions/repeat keys.
     public boolean put(String key, String value){
-        return true;
+        if (get(key) == null){
+            System.out.println(hashCode(key) % size);
+            table.set(hashCode(key) % size, value);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //get hashes the key to get the index, and returns that element. Returns null if key not found.
     public String get(String key){
-        return "";
+        return table.get(hashCode(key) % size);
     }
 
     //returns the unique int in the range of the [0, array length)
@@ -28,5 +39,9 @@ public class HashTable<E>{
             index++;
         }
         return hash % 2027;
+    }
+
+    public String toString(){
+        return table.toString();
     }
 }
--- src/testDriver.java
+++ src/testDriver.java
@@ -1,15 +1,17 @@
 public class testDriver {
     public static void main(String[] args) {
-        HashTable<String> ht = new HashTable<>(10);
-        System.out.println(ht.hashCode("aaaa"));
-        System.out.println(ht.hashCode("aaab"));
-        System.out.println(ht.hashCode("bbbb"));
-        System.out.println(ht.hashCode("cccc"));
-        System.out.println(ht.hashCode("dddd"));
-        System.out.println(ht.hashCode("House"));
-        System.out.println(ht.hashCode("housE"));
-
-        //where it maxes out
-        System.out.println(ht.hashCode("superduperduperduperdddd"));
+        HashTable ht = new HashTable(10);
+        System.out.println(ht.put("1", "a"));
+        System.out.println(ht.put("2", "b"));
+        System.out.println(ht.put("1", "a"));
+        System.out.println(ht.put("3", "c"));
+        System.out.println(ht.put("4", "d"));
+        System.out.println(ht.put("5", "e"));
+        System.out.println(ht.put("6", "f"));
+        System.out.println(ht.put("7", "g"));
+        System.out.println(ht.put("8", "h"));
+        System.out.println(ht.put("9", "i"));
+        System.out.println(ht.put("10", "j"));
+        System.out.println(ht);
     }
 }
